4aa75007eff603a5b58f20f73d34d593c4f6f461
rmnvnc <romanvanco23@gmail.com>
2025-07-21 10:58:29 +0200

    cleaning - change editorconfig, remove unused views

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import AuthView from '../views/AuthView.vue'
 
 const router = createRouter({
-	history: createWebHistory(import.meta.env.BASE_URL),
-	routes: [
-		{
-			path: '/',
-			name: 'home',
-			component: HomeView,
-		},
-		{
-			path: '/auth',
-			name: 'userAuth',
-			component: () => import('../views/AuthView.vue'),
-		},
-	],
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'userAuth',
+            component: AuthView,
+        },
+    ],
 })
 
 export default router
